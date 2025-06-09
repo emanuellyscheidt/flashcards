4bc33516a6954ce8934a1473be1152361f1ec75f
emanuellyscheidt <emanuelly.scheidt@escola.pr.gov.br>
2025-06-09 15:01:49 +0000

    ok

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function criaCartao(categoria, pergunta, resposta){
   let respostaEstaVisivel = false
   function viraCartao(){
       respostaEstaVisivel = !respostaEstaVisivel
-      cartao.classList.toogle('active',respostaEstaVisivel)
+      cartao.classList.toggle('active',respostaEstaVisivel)
   }
   cartao.addEventListener('click',viraCartao)
   container.appendChild(cartao)
